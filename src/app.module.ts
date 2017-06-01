5eefa629051d3276bf37eba800c1fb010b58991c
Hassona75 <moh.hassona244@gmail.com>
2017-06-01 20:27:04 +0200

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,13 +11,14 @@ import { Home } from './home';
 import { Login } from './login';
 import { Signup } from './signup';
 import { App } from './app';
+import { Hamada } from './hamada';
 
 import { routes } from './app.routes';
 
 @NgModule({
   bootstrap: [App],
   declarations: [
-    Home, Login, Signup, App
+    Home, Login, Signup, App , Hamada
   ],
   imports: [
     HttpModule, BrowserModule, FormsModule,
--- src/app.routes.ts
+++ src/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './home';
 import { Login } from './login';
 import { Signup } from './signup';
+import { Hamada } from './hamada';
 import { AuthGuard } from './common/auth.guard';
 
 export const routes: Routes = [
@@ -9,5 +10,6 @@ export const routes: Routes = [
   { path: 'login',  component: Login },
   { path: 'signup', component: Signup },
   { path: 'home',   component: Home, canActivate: [AuthGuard] },
+  { path: 'hamada',   component: Hamada },
   { path: '**',     component: Login },
 ];
