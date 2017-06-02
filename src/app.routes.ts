54aff3985313e5b4a296953fd0725d7afb866432
Hassona75 <moh.hassona244@gmail.com>
2017-06-02 13:28:23 +0200

    c

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -2,14 +2,10 @@ import { Routes } from '@angular/router';
 import { Home } from './home';
 import { Login } from './login';
 import { Signup } from './signup';
-<<<<<<< HEAD
 import { Hamada } from './hamada';
 import { Hamada2Component } from './hamada2/hamada2.component';
-=======
 import { Header } from './header';
 import { Footer } from './footer';
-
->>>>>>> 78951647c47eb2bd2c270b6173ca3847ced98278
 import { AuthGuard } from './common/auth.guard';
 
 export const routes: Routes = [
@@ -17,12 +13,9 @@ export const routes: Routes = [
   { path: 'login',  component: Login },
   { path: 'signup', component: Signup },
   { path: 'home',   component: Home, canActivate: [AuthGuard] },
-<<<<<<< HEAD
   { path: 'hamada',   component: Hamada },
   { path: 'hamada2',   component: Hamada2Component },
-=======
   { path: 'header',   component: Header },
   { path: 'footer',   component: Footer },
->>>>>>> 78951647c47eb2bd2c270b6173ca3847ced98278
   { path: '**',     component: Login },
 ];
