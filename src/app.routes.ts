f4a38304316673575f34ca0a91a88f632d5f2df8
asmaa-elzeiny <asmaa_elzeiny@ieee-zsb.org>
2017-06-02 16:06:54 +0200

    c

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -13,16 +13,10 @@ export const routes: Routes = [
   { path: '',  component: Login },
   { path: 'login', component: Login },
   { path: 'signup', component: Signup },
-<<<<<<< HEAD
-  { path: 'home', component: Home, canActivate: [AuthGuard] },
-  { path: 'hamada', component: Hamada },
   { path: 'pm-operations', component: OperationsComponent },
-  { path: '**', component: Login },
-=======
   { path: 'home',   component: Home, canActivate: [AuthGuard] },
   { path: 'header',   component: Header },
   { path: 'footer',   component: Footer },
   { path: 'profile',  component: Profile },
-  { path: '**',     component: Login },
->>>>>>> 843bfb544abaa8c24e6d339a16b9e6bcb6e22498
+  { path: '**',     component: Login }
 ];
