40b354a63a7a81581473f9934c11fdc3bc6cff26
Hassona75 <moh.hassona244@gmail.com>
2017-06-02 13:58:51 +0200

    c

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,25 +11,17 @@ import { Home } from './home';
 import { Login } from './login';
 import { Signup } from './signup';
 import { App } from './app';
-<<<<<<< HEAD
-import { Hamada } from './hamada';
 import { Hamada2Component } from './hamada2/hamada2.component';
 
-=======
 import { Header } from './header';
 import {Footer} from './footer';
->>>>>>> 78951647c47eb2bd2c270b6173ca3847ced98278
 import { routes } from './app.routes';
 
 @NgModule({
   bootstrap: [App],
   declarations: [
-<<<<<<< HEAD
-    Home, Login, Signup, App , Hamada , Hamada2Component
-=======
-    Home, Login, Signup, App , Header , Footer
->>>>>>> 78951647c47eb2bd2c270b6173ca3847ced98278
-  ],
+    Home, Login, Signup, App  , Hamada2Component
+    ],
   imports: [
     HttpModule, BrowserModule, FormsModule,
     RouterModule.forRoot(routes, {
