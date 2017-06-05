f84875d99fff4b4ff7f22178466d3ed651b1e667
Kireya-Atef <kireyaattia@gmail.com>
2017-06-05 12:37:22 +0200

    kireya-merge

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -2,17 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './home';
 import { Login } from './login';
 import { Signup } from './signup';
-import { Header } from './header';
-<<<<<<< HEAD
+
 import{ItemsComponent} from'./items/item.component'
 import{DetailComponent} from'./item details/detail.component'
 
-import { Footer } from './footer';
-=======
-import { Footer } from './footer';
 import { Profile } from './profile';
-import { ToolCategory} from './toolCategory'
->>>>>>> fc5a46e19821a49344321896880b390b06dd8fab
+import { ToolCategory} from './toolCategory';
 import { AuthGuard } from './common/auth.guard';
 import{Needs} from './needs';
 
@@ -21,10 +16,8 @@ export const routes: Routes = [
   { path: 'login',  component: Login },
   { path: 'signup', component: Signup },
   { path: 'home',   component: Home, canActivate: [AuthGuard] },
-  { path: 'header',   component: Header },
   { path: 'items',   component:ItemsComponent },
   { path: 'detail',   component:DetailComponent },  
-  { path: 'footer',   component: Footer },
   { path: 'profile',  component: Profile },
   { path: 'toolCategory',  component: ToolCategory },
   {path:'needs',component:Needs},
