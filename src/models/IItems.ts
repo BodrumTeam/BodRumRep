886e264042a19dabc2f23c0274d446445d72a231
Hassona75 <moh.hassona244@gmail.com>
2017-06-14 17:41:58 +0200

    c

--- src/models/IItems.ts
+++ src/models/IItems.ts
@@ -1,10 +1,8 @@
-import {IUsers} from "../models/IUsers"
 export interface IITem{
         id:Number;
-        title:string,
-        description:String,
-        price:number , 
-        photo:string,
-        date:string,
-        fk_userId:number;
+        title: string,
+        description: String,
+        price: number , 
+        photo : string,
+        date: string
 }
